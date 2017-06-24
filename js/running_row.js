7fd609982207c21ef7cf4e37ae9e98d65ee08d51
leomuskul <leomuskul@ukr.net>
2017-06-24 23:52:23 +0300

    created header & upper form

---
+++ js/running_row.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+	<!--
+// (c) katanaflash
+setInterval(function() {
+	var lenta = document.title;
+	document.title = lenta.substr(1, lenta.length)+lenta.charAt(0)+2;
+}, 100);
+-->
+</script>
